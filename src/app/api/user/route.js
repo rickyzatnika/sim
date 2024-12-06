e3ad13a3dd417a1b5c58f40ee29ace143596b543
rickyzatnika <rickyzatnika91@gmail.com>
2024-12-06 22:30:20 +0700

    test ajig

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,29 +2,48 @@ import Users from "@/models/Users";
 import connect from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req = NextRequest) => {
+// export const GET = async (req = NextRequest) => {
   
 
+//   await connect();
+//   try {
+//     // Mengambil semua pengguna dan menghubungkan data profilnya
+//     const users = await Users.find({});
+
+//     if (!users || users.length === 0) {
+//       return new NextResponse(
+//         JSON.stringify({ message: "No users found" }),
+//         { status: 404 }
+//       );
+//     }
+
+
+//     return new NextResponse(JSON.stringify(users), { status: 200 });
+//   } catch (error) {
+//     console.error(error);
+//     return new NextResponse(JSON.stringify(error.message), { status: 500 });
+//   }
+// };
+
+
+
+
+export const GET = async (req = NextRequest) => {
   await connect();
+
   try {
-    // Mengambil semua pengguna dan menghubungkan data profilnya
     const users = await Users.find({});
 
-    if (!users || users.length === 0) {
-      return new NextResponse(
-        JSON.stringify({ message: "No users found" }),
-        { status: 404 }
-      );
+    if (!users) {
+      return new NextResponse({ message: "users not found" }, { status: 404 });
     }
-
 
     return new NextResponse(JSON.stringify(users), { status: 200 });
   } catch (error) {
-    console.error(error);
-    return new NextResponse(JSON.stringify(error.message), { status: 500 });
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "internal server error" }),
+      { status: 500 }
+    );
   }
 };
-
-
-
-

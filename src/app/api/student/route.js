06e23384ce5bd12f3eeb6459d2fec7e6be395412
rickyzatnika <rickyzatnika91@gmail.com>
2024-12-06 22:57:11 +0700

    test

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -1,44 +1,22 @@
 import Users from "@/models/Users";
 import connect from "@/utils/connect";
-import { NextRequest, NextResponse } from "next/server";
-
-// export const GET = async (req = NextRequest) => {
-  
-
-//   await connect();
-//   try {
-//     // Mengambil semua pengguna dan menghubungkan data profilnya
-//     const users = await Users.find({});
-
-//     if (!users || users.length === 0) {
-//       return new NextResponse(
-//         JSON.stringify({ message: "No users found" }),
-//         { status: 404 }
-//       );
-//     }
-
-
-//     return new NextResponse(JSON.stringify(users), { status: 200 });
-//   } catch (error) {
-//     console.error(error);
-//     return new NextResponse(JSON.stringify(error.message), { status: 500 });
-//   }
-// };
+import { NextResponse } from "next/server";
 
 
 
 
-export async function GET (req = NextRequest) {
+
+export async function GET (req) {
   await connect();
 
   try {
-    const users = await Users.find({});
+    const user = await Users.find({});
 
-    if (!users) {
+    if (!user) {
       return new NextResponse({ message: "users not found" }, { status: 404 });
     }
 
-    return new NextResponse(JSON.stringify(users), { status: 200 });
+    return new NextResponse(JSON.stringify(user), { status: 200 });
   } catch (error) {
     console.log(error);
     return new NextResponse(

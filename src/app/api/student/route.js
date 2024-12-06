9d2641af4f3f2b03537f26028a7f49aeb0638a3d
rickyzatnika <rickyzatnika91@gmail.com>
2024-12-06 22:52:57 +0700

    test async function

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -28,7 +28,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-export const GET = async (req = NextRequest) => {
+export async function GET (req = NextRequest) {
   await connect();
 
   try {
